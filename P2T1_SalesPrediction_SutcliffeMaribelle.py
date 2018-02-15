e9c383033dead83a16cc3119d0d76fdd9c3d51e2
sutclifm0850 <36495772+sutclifm0850@users.noreply.github.com>
2018-02-14 23:09:01 -0500

    First submission

---
+++ P2T1_SalesPrediction_SutcliffeMaribelle.py
@@ -0,0 +1,10 @@
+# CTI-110
+# P2T1 - Sales Prediction
+# Maribelle Sutcliffe
+# February 14, 2018
+
+totalSales = float(input('Enter the projected sales: '))
+annualProfit = totalSales * 0.23
+print('The profit is $', format(annualProfit, ',.2f'))
+
+      
